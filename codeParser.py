c589d018fa5f630d7228135043ac73408bdeb93e
Paras Munoli <parasmunoli@gmail.com>
2025-01-06 12:58:39 +0530

    Function to Extract function features from code

--- codeParser.py
+++ codeParser.py
@@ -10,4 +10,14 @@ class CodeParser(ast.NodeVisitor):
         self.docStrings = []
         self.code = []
 
+    def visitFunctionDef(self, node):
+        docString = ast.get_docstring(node)
+        functionInfo = {
+            "name": node.name,
+            "docString": docString,
+            "args": [arg.arg for arg in node.args.args],
+            "returns": getattr(node.returns, "id", None) if node.returns else None
+        }
+        self.functions.append(functionInfo)
+        self.generic_visit(node)
 

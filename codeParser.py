a4e1ef9795d6f8c4311efa29baaf551b900df0ee
Paras Munoli <parasmunoli@gmail.com>
2025-01-06 13:21:00 +0530

    Updated function names for compatability with ast

--- codeParser.py
+++ codeParser.py
@@ -7,17 +7,38 @@ class CodeParser(ast.NodeVisitor):
         self.imports = []
         self.variables = []
         self.comments = []
-        self.docStrings = []
+        self.docstrings = []
         self.code = []
 
-    def visitFunctionDef(self, node):
-        docString = ast.get_docstring(node)
-        functionInfo = {
+    def visit_FunctionDef(self, node):
+        docstring = ast.get_docstring(node)
+        function_info = {
             "name": node.name,
-            "docString": docString,
+            "docstring": docstring,
             "args": [arg.arg for arg in node.args.args],
             "returns": getattr(node.returns, "id", None) if node.returns else None
         }
-        self.functions.append(functionInfo)
+        self.functions.append(function_info)
         self.generic_visit(node)
+
+    def visit_ClassDef(self, node):
+        docstring = ast.get_docstring(node)
+        class_info = {
+            "name": node.name,
+            "base_classes": [base.id for base in node.bases if hasattr(base, "id")],
+            "docstring": docstring,
+            "methods": []
+        }
+        for item in node.body:
+            if isinstance(item, ast.FunctionDef):
+                method_docstring = ast.get_docstring(item)
+                method_info = {
+                    "name": item.name,
+                    "args": [arg.arg for arg in item.args.args],
+                    "docstring": method_docstring
+                }
+                class_info["methods"].append(method_info)
+        self.classes.append(class_info)
+        self.generic_visit(node)
+
 

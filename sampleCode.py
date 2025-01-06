edfeafec8425d81505a19241579c8bd4d15399fb
Paras Munoli <parasmunoli@gmail.com>
2025-01-06 13:31:13 +0530

    Created sampleCode.py for testing

---
+++ sampleCode.py
@@ -0,0 +1,15 @@
+# This is a sample comment
+class Example:
+    """Example class docstring."""
+    def __init__(self, value):
+        """Initialize the class."""
+        self.value = value
+
+    def add(self, x):
+        """Add a number to the value."""
+        return 20 + x
+
+# A standalone function
+def greet(name):
+    """Greet a user by name."""
+    return f"Hello, {name}!"

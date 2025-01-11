7ba83d2c4577b0c34cfb75565049008e09a433df
Paras Munoli <parasmunoli@gmail.com>
2025-01-11 16:04:01 +0530

    Tried to add more features but didnot work

--- llmParserCPP.py
+++ llmParserCPP.py
@@ -1,58 +1,126 @@
-import clang.cindex
 import json
+
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
+
 class CPPFeatureExtractor:
-    def __init__(self, program_name="Program Analysis"):
-        self.program_name = program_name
+    def __init__(self, model_name="t5-small"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+        self.program_name = "Program Analysis"
         self.features = {
-            "Classes": [],
-            "Functions": [],
-            "Variables": [],
+            "Variables": {
+                "Declaration": [],
+                "Initialization": [],
+                "Global Variables": [],
+                "Local Variables": [],
+                "Constants": []
+            },
+            "Data Types": {
+                "Basic": {"int": 0, "float": 0, "double": 0, "char": 0},
+                "Derived": {"Arrays": 0, "Pointers": 0, "Functions": 0},
+                "Enumeration": [],
+                "User-defined": {"Struct": [], "Union": []}
+            },
+            "Operators": {
+                "Arithmetic": [],
+                "Assignment": [],
+                "Relational": [],
+                "Logical": [],
+                "Bitwise": [],
+                "Increment/Decrement": [],
+                "Conditional (Ternary)": [],
+                "Special": []
+            },
+            "Control Flow": {
+                "if statement": 0,
+                "else statement": 0,
+                "switch-case": 0,
+                "for loop": 0,
+                "while loop": 0,
+                "do-while loop": 0
+            },
+            "Functions": {
+                "Function Declaration": [],
+                "Function Definition": [],
+                "Call by Value": [],
+                "Call by Reference": [],
+                "Inline Functions": []
+            },
+            "Pointers": {
+                "Declaration and Initialization": [],
+                "Pointer Arithmetic": [],
+                "Pointers to Arrays": [],
+                "Pointers to Functions": [],
+                "Dynamic Memory Allocation": []
+            },
+            "Arrays": {
+                "Single Dimensional Arrays": [],
+                "Multidimensional Arrays": [],
+                "Arrays of Pointers": []
+            },
+            "Strings": {
+                "String Handling Functions": [],
+                "Character Arrays": []
+            },
+            "Object-Oriented Programming (C++)": {
+                "Classes and Objects": [],
+                "Constructors and Destructors": [],
+                "Inheritance": [],
+                "Polymorphism": {
+                    "Compile-time": [],
+                    "Runtime": []
+                },
+                "Encapsulation": [],
+                "Abstraction": []
+            },
+            "File Handling": {
+                "File Opening Modes": [],
+                "Reading/Writing to Files": [],
+                "Closing Files": []
+            },
+            "Standard Template Library (STL - C++)": {
+                "Containers": {
+                    "Vectors": [],
+                    "Lists": [],
+                    "Sets": [],
+                    "Maps": [],
+                    "Queues": []
+                },
+                "Iterators": [],
+                "Algorithms": []
+            },
+            "Preprocessor Directives": {
+                "#define": [],
+                "#include": [],
+                "#ifdef/#ifndef": [],
+                "#undef": [],
+                "#pragma": [],
+                "Macros": []
+            },
+            "Debugging": {
+                "printf and cerr": [],
+                "GDB (GNU Debugger)": [],
+                "Valgrind (Memory Debugging)": []
+            },
+            "Others": {
+                "Recursion": [],
+                "Multithreading": [],
+                "Dynamic Memory Allocation": [],
+                "const Keyword": [],
+                "Volatile Keyword": []
+            }
         }
 
     def extract_features(self, code):
-        # Use Clang's libtooling for parsing
-        index = clang.cindex.Index.create()
-        tu = index.parse("temp.cpp", unsaved_files=[("temp.cpp", code)])
-        for node in tu.cursor.get_children():
-            self._process_node(node)
+        prompt = f"Extract C++ features from the following code:\n\n{code}"
+        inputs = self.tokenizer(prompt, return_tensors="pt", truncation=True)
+        outputs = self.model.generate(**inputs, max_length=512)
+        result = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        # Parsing the result (assumes output is in JSON format)
+        try:
+            extracted_features = json.loads(result)
+            self.features.update(extracted_features)
+        except json.JSONDecodeError:
+            print("Failed to parse the model output. Returning features as-is.")
         return self.features
-
-    def _process_node(self, node):
-        if node.kind == clang.cindex.CursorKind.CLASS_DECL:
-            self.features["Classes"].append({"name": node.spelling, "methods": []})
-        elif node.kind == clang.cindex.CursorKind.FUNCTION_DECL:
-            self.features["Functions"].append({"name": node.spelling, "return_type": node.result_type.spelling})
-        elif node.kind == clang.cindex.CursorKind.VAR_DECL:
-            self.features["Variables"].append({"name": node.spelling, "type": node.type.spelling})
-
-class CPPAnalysis:
-    def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("t5-small")
-        self.model = AutoModelForSeq2SeqLM.from_pretrained("t5-small")
-
-    def analyze_code(self, code):
-        extractor = CPPFeatureExtractor()
-        features = extractor.extract_features(code)
-        return features
-
-if __name__ == "__main__":
-    sample_cpp_code = """
-    class Calculator {
-    public:
-        int add(int a, int b);
-        int subtract(int a, int b);
-    };
-
-    int Calculator::add(int a, int b) {
-        return a + b;
-    }
-
-    int Calculator::subtract(int a, int b) {
-        return a - b;
-    }
-    """
-    analyzer = CPPAnalysis()
-    result = analyzer.analyze_code(sample_cpp_code)
-    print(json.dumps(result, indent=4))

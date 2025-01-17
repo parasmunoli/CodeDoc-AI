6366167f93ae9a1d412224f078270d37e93c8c33
Paras Munoli <parasmunoli@gmail.com>
2025-01-17 13:49:15 +0530

    Added function for status and landing page

--- CodeDocAI/mainApp/views.py
+++ CodeDocAI/mainApp/views.py
@@ -1,3 +1,16 @@
+# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def status(request):
+    return JsonResponse({'success': True, 'code': 200})
+
+def home(request):
+    from django.http import HttpResponseRedirect
+    return HttpResponseRedirect('https://codedocailt4y.rollout.site/')
+
+def login(request):
+    return render(request, 'login.html')
+

3973d4385f4a69384deb642c8181fcfe822f22d8
Paras Munoli <parasmunoli@gmail.com>
2025-01-22 12:04:16 +0530

    Updated the Documentation generation

--- model/documentGeneration.py
+++ model/documentGeneration.py
@@ -1,6 +1,7 @@
+from click import prompt
 from markdownify import markdownify
 from meta_ai_api import MetaAI
-
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # from transformers import MarkupLMFeatureExtractor, MarkupLMTokenizerFast, MarkupLMProcessor
 
@@ -14,34 +15,100 @@ class CodeDocumentationGenerator:
 
     def generate_full_documentation(self, link):
         """Generate full documentation using features."""
-        print("Generating full documentation...")
+        print("Generating Basic Skeleton for documentation...")
         prompt = (
-            f"""Return the response in HTML format only. Ensure the response is detailed and comprehensive, covering all the sections listed explicitly
-{link}, visit the GitHub repository and create a detailed documentation that includes:
-Project Overview: Provide a elaborated brief overview of the project, including its purpose, goals, and objectives.
-Technologies Used: Read the code files from the repository only and identify the technologies that are used in the project do not include python if not used in repo link
-Requirements: Read the code files and describe the requirements for the project. 
-Installation Instructions: Read the and analyze all the code and Provide detailed installation instructions for the project. Include the following steps:
-Clone the repository using the command and link is {link}
-Navigate to the project directory using the command
-Install the required dependencies using the command
-Configure the project settings as needed
-Usage Instructions provide the details such as Code file from the link repository name to run the project and the command to run it and if docker file available available use docker to spin it up
-Provide detailed usage instructions for the project.
-Read code and run the project using the command
-Acknowledgments
-Provide acknowledgments for contributors, including:
-List of Contributors
-Contributions Made
-Conclusion
-Conclude the project with a summary, including:
-Project Overview
-Key Features"""
+            f"""response only in HTML code visit {link} and generate Project title only for the project do not generate any description use <h1>."""
         )
-        response = self.text_generator.prompt(prompt)
-        print("Full documentation generated successfully!")
-        return response
+        basicDoc = self.text_generator.prompt(prompt)
+        description = self.elaboratedDesc(link)
+        toc = '''<h2>Table of Contents</h2>
+                <li>Install and Run the Project</li>
+                <li>Use the Project</li>
+                <li>Credits</li>
+                <li>License</li>
+                <li>Contribution</li>'''
+        cloneCommand = self.cloneCommand(link)
+        usage = self.usage(link)
+        credits = self.credits(link)
+        contribution = self.contributionInstruct(link)
+        markdown_doc = self.convertToMarkdown(basicDoc.get('message', '') + description.get('message', '') + '<h1>Getting Started</h1>' + toc + cloneCommand + usage.get('message', '') + credits.get('message', '') + contribution.get('message', ''))
+        return markdown_doc
 
     def convertToMarkdown(self, text):
-        markdownCode = markdownify(text)
-        return markdownCode
+        return markdownify(text)
+
+    def elaboratedDesc(self,link):
+        print("Elaborating description...")
+        prompt = (
+            f"""response in html code only Detailed Elaborate the description of the project: {link}
+            """
+        )
+        description = self.text_generator.prompt(prompt)
+        print("Elaborated description generated successfully!")
+        return description
+
+    def toc(self):
+        print("Generating Table of Contents...")
+        return f'''<h2>Table of Contents</h2>
+                <li>Install and Run the Project</li>
+                <li>Use the Project</li>
+                <li>Credits</li>
+                <li>License</li>
+                <li>Contribution</li>'''
+
+    def cloneCommand(self,link):
+        print("Generating Clone Command...")
+        clonePrompt = (
+            f"""Analyze the code files and programming languages used in the repository and provide grouped Install dependencies for the project based on the files in the repository: {link} provide one by one in html code"""
+        )
+        description = self.text_generator.prompt(clonePrompt)
+        print("Clone Command generated successfully!")
+        return f'''<h3>Installation</h3>
+                <ol>
+                    <li><strong>Clone the Repository</strong>:
+                        <pre><code>git clone {link}.git
+                cd CodeDoc-AI</code></pre>
+                    </li>
+                    <li><strong>Install Dependencies</strong>:
+                        {description.get('message', '')}
+                    </li>
+                </ol>'''
+
+    def usage(self,link):
+        print('Generating Usage...')
+        prompt = f'''Analyze the github repository and provide one by one in html code
+                    {link} provide the usage of the project. do not include installation and clone command'''
+        usage = self.text_generator.prompt(prompt)
+        print("Usage generated successfully!")
+        return usage
+
+    def credits(self,link):
+        print("Generating Credits...")
+        prompt = f'''Findall the credits for the project: {link} do not generate licenses'''
+        creds = self.text_generator.prompt(prompt)
+        print("Credits generated successfully!")
+        return creds
+
+    def contributionInstruct(self,link):
+        print("Generating Contribution...")
+        prompt = f'''Provide only the contribution instructions for the project: {link} response in html code only.'''
+        contribution = self.text_generator.prompt(prompt)
+        print("Contribution generated successfully!")
+        return contribution
+
+    def finalCodeDoc(self, text):
+        print("Converting to Markdown...")
+        print("Initialising text model...")
+        model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+        tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+        print("Text model initialised successfully!")
+        inputs = tokenizer.encode("expand: " + text, return_tensors="pt")
+        outputs = model.generate(inputs, max_length=2048, num_beams=4, early_stopping=True)
+        elaborated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+        print("Text converted to Markdown successfully!")
+        return elaborated_text
+
+if __name__ == "__main__":
+    generator = CodeDocumentationGenerator()
+    documentation = generator.generate_full_documentation("https://github.com/NILESHD2003/HLS-Adaptive_Bitrate_Streaming")
+    print("HTML Doc: ",documentation)

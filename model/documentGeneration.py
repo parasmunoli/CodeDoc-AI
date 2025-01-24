e8678b32298b4882832d92e318d304b849800ff9
Paras Munoli <parasmunoli@gmail.com>
2025-01-24 11:32:33 +0530

    Updated code

--- model/documentGeneration.py
+++ model/documentGeneration.py
@@ -1,4 +1,3 @@
-from click import prompt
 from markdownify import markdownify
 from meta_ai_api import MetaAI
 from transformers import T5Tokenizer, T5ForConditionalGeneration
@@ -10,7 +9,7 @@ class CodeDocumentationGenerator:
         self.text_generator = MetaAI()
         print("AI text generator initialized successfully!")
 
-    def generate_full_documentation(self, link):
+    def generate_full_documentation(self, link, projectdesc):
         """Generate full documentation using features."""
         print("Generating Basic Skeleton for documentation...")
         prompt = (
@@ -24,23 +23,22 @@ class CodeDocumentationGenerator:
                 <li>Credits</li>
                 <li>License</li>
                 <li>Contribution</li>'''
-        cloneCommand = self.cloneCommand(link)
+        cloneCommand = self.cloneCommand(link, projectdesc)
         usage = self.usage(link)
-        credits = self.credits(link)
+        credit = self.credits(link)
         contribution = self.contributionInstruct(link)
-        markdown_doc = self.convertToMarkdown(basicDoc.get('message', '') + description.get('message', '') + '<h1>Getting Started</h1>' + toc + cloneCommand + usage.get('message', '') + credits.get('message', '') + contribution.get('message', ''))
+        markdown_doc = self.convertToMarkdown(basicDoc.get('message', '') + description.get('message', '') + '<h1>Getting Started</h1>' + toc + cloneCommand + usage.get('message', '') + credit.get('message', '') + contribution.get('message', ''))
         return markdown_doc
-
+    @staticmethod
     def convertToMarkdown(self, text):
         return markdownify(text)
 
     def elaboratedDesc(self,link):
         print("Elaborating description...")
-        prompt = (
-            f"""response in html code only Detailed Elaborate the description of the project: {link}
-            """
+        descprompt = (
+            f"""response in html code only Detailed Elaborate the description of the project: {link}"""
         )
-        description = self.text_generator.prompt(prompt)
+        description = self.text_generator.prompt(descprompt)
         print("Elaborated description generated successfully!")
         return description
 
@@ -49,14 +47,15 @@ class CodeDocumentationGenerator:
         return f'''<h2>Table of Contents</h2>
                 <li>Install and Run the Project</li>
                 <li>Use the Project</li>
+                <li>Project Structure</li>    
                 <li>Credits</li>
                 <li>License</li>
                 <li>Contribution</li>'''
 
-    def cloneCommand(self,link):
+    def cloneCommand(self,link, projectdesc):
         print("Generating Clone Command...")
         clonePrompt = (
-            f"""Analyze the code files and programming languages used in the repository and provide grouped Install dependencies for the project based on the files in the repository: {link} provide one by one in html code"""
+            f"""Analyze the code files and programming languages used in the repository and provide grouped Install dependencies for the project based on the files in the repository: {link} provide one by one in html code. {projectdesc} use this description to generate the clone command."""
         )
         description = self.text_generator.prompt(clonePrompt)
         print("Clone Command generated successfully!")
@@ -73,16 +72,16 @@ class CodeDocumentationGenerator:
 
     def usage(self,link):
         print('Generating Usage...')
-        prompt = f'''Analyze the github repository and provide one by one in html code
+        usageprompt = f'''Analyze the github repository and provide one by one in html code
                     {link} provide the usage of the project. do not include installation and clone command'''
-        usage = self.text_generator.prompt(prompt)
+        usage = self.text_generator.prompt(usageprompt)
         print("Usage generated successfully!")
         return usage
 
     def credits(self,link):
         print("Generating Credits...")
-        prompt = f'''Findall the credits for the project: {link} do not generate licenses'''
-        creds = self.text_generator.prompt(prompt)
+        creditprompt = f'''Findall the credits for the project: {link} do not generate licenses'''
+        creds = self.text_generator.prompt(creditprompt)
         print("Credits generated successfully!")
         return creds
 
@@ -106,7 +105,8 @@ class CodeDocumentationGenerator:
         return elaborated_text
 
 # Uncomment to try and test the code
-# if __name__ == "__main__":
-#     generator = CodeDocumentationGenerator()
-#     documentation = generator.generate_full_documentation("<Github Link>")
-#     print("HTML Doc: ",documentation)
+if __name__ == "__main__":
+    generator = CodeDocumentationGenerator()
+    projectdesc = '''This is a completely python project which generate a detailed documentation for the project. the backend is handeled using django'''
+    documentation = generator.generate_full_documentation("https://github.com/parasmunoli/CodeDoc-AI",projectdesc)
+    print("HTML Doc: ",documentation)
